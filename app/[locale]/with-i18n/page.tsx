870e3ccf5cbfc174f7d021c56216c400438f56f9
enterwiz <enterwizdev@outlook.com>
2025-07-12 05:48:23 +0800

    first commit

---
+++ app/[locale]/with-i18n/page.tsx
@@ -0,0 +1,3 @@
+export default function WithI18n() {
+  return <div>with i18n page, please check the canonical link!</div>;
+}
---
+++ app/without-i18n/page.tsx
@@ -0,0 +1,3 @@
+export default function WithoutI18n() {
+  return <div>without i18n page, please check the canonical link!</div>;
+}
